150e12bfb0e46e7402d59b0ec4294c722c033439
pranav-k-jha <pranav.jha@mail.concordia.ca>
2023-11-08 12:18:15 -0500

    added countries details page

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const countriesContainer = document.querySelector(".countries-container");
 fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) =>
-    data.map((data) => {
-      console.log(data.capital);
+    data.map((country) => {
+      console.log(country.capital);
       const countryCard = document.createElement("a");
       countryCard.classList.add("country-card");
 
       countryCard.innerHTML = `
-        <img src=${data.flags.svg} alt="flag-canada" />
+        <img src=${country.flags.svg} alt=${country.name.common} flag/>
         <div class="card-text">
-            <h3 class="card-title">${data.name.common}</h3>
-            <p><b>Capital: </b>${data.capital}</p>
-            <p><b>Population: </b>${data.population.toLocaleString()}</p>
-            <p><b>Region: </b>${data.region}</p>
+            <h3 class="card-title">${country.name.common}</h3>
+            <p><b>Capital: </b>${country.capital}</p>
+            <p><b>Population: </b>${country.population.toLocaleString()}</p>
+            <p><b>Region: </b>${country.region}</p>
         </div>`;
 
       countriesContainer.append(countryCard);
