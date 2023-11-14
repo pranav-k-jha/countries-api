85897159d6beed65907df7b84936c0156aa5c549
pranav-k-jha <pranav.jha@mail.concordia.ca>
2023-11-14 16:50:46 -0500

    added functionality to fetch and show each country data

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const countriesContainer = document.querySelector(".countries-container");
 
 fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
-  .then((data) =>
+  .then((data) => 
     data.map((country) => {
+      if(country.capital){
       console.log(country.capital);
+      }
       const countryCard = document.createElement("a");
       countryCard.classList.add("country-card");
-
+      countryCard.href = `/country.html?name=${country.name.common}`;
       countryCard.innerHTML = `
         <img src=${country.flags.svg} alt=${country.name.common} flag/>
         <div class="card-text">
