bcfa3012737adc67c1d8ceced6389024fa0b1164
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-05-05 19:58:22 -0400

    updated number format to Intl

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function renderCountries(data) {
           <div class="card-text">
               <h3 class="card-title">${country.name.common}</h3>
               <p><b>Population: </b>${country.population.toLocaleString(
-                "en-IN"
+                "en"
               )}</p>
               <p><b>Region: </b>${country.region}</p>
               <p><b>Capital: </b>${country.capital?.[0]}</p>
